9447f9ae5ff13b46455f7c31fae9a230dbcbebd6
Pedro Fontana <fontana.pedro93@gmail.com>
2023-07-17 10:36:05 -0300

    vol c

---
+++ vol.c
@@ -0,0 +1,14 @@
+/* Computes the dimensions of a box from input provided by the user */
+#include <stdio.h>
+int main(void)
+{
+  int height, length, width, volume, weight;
+  printf("Enter height of box: ");
+  scanf("%d", &height);
+  printf("Enter length of box: ");
+  scanf("%d", &length);
+  printf("Enter width of box: ");
+  scanf("%d", &width);
+  volume = height * length * width;
+  printf("Volume (cubic inches): %d\n", volume);
+return 0; }
